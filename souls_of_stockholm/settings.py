66f38ee900f926640100bc45d97d3b7a638a9ecd
KatanaNevermore <grenkachannel@hotmail.com>
2023-10-24 09:35:23 +0300

    fix

--- souls_of_stockholm/settings.py
+++ souls_of_stockholm/settings.py
@@ -151,7 +151,7 @@ AUTH_USER_MODEL = 'user.CustomUser'
 STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
